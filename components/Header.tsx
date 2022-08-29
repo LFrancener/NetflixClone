2b6729a6a0a3dfacc668bd46483f0a4493b1e358
LFrancener <luciano.francener7@gmail.com>
2022-08-28 22:36:02 -0300

    Created Row and Thumbnail components.

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ function Header() {
           src="https://rb.gy/ulxxee"
           width={100}
           height={100}
-          className="cursor-pointer object-contain"
+          className="object-contain cursor-pointer"
         />
         <ul className="hidden space-x-2 md:flex">
           <li className="headerLink">Home</li>
@@ -35,14 +35,14 @@ function Header() {
         </ul>
       </div>
       <div className="flex items-center space-x-4 text-sm font-light">
-        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <SearchIcon className="hidden w-6 h-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
-        <BellIcon className="h-6 w-6" />
+        <BellIcon className="w-6 h-6" />
         <Link href="/account">
           <img
             src="https://rb.gy/g1pwyx"
             alt=""
-            className="cursor-pointer rounded"
+            className="rounded cursor-pointer"
           />
         </Link>
       </div>
